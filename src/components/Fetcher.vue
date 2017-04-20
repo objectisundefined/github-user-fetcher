8796f88d638cf30695edd63ca91c9294e8bda0c4
objectisundefined <2535612040@qq.com>
2017-04-20 15:54:21 +0800

    Feature: v2

--- src/components/Fetcher.vue
+++ src/components/Fetcher.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
-    <form @submit.prevent="search">
-      <input v-model="username" placeholder="Enter a github username!">
+  <div class="fetch">
+    <form @submit.prevent="submit">
+      <input v-model="inputValue" placeholder="Enter a github username...">
+      <button>Go!</button>
     </form>
-    <p v-if="data.name && data.location">
-      {{ data.name }} ({{ data.login }})
-      is from
-      {{ data.location }}
-    </p>
-    <p v-else>{{ errorMsg }}</p>
+    <h1 v-text="username"></h1>
+    <img v-if="avatar" :src="avatar" class="avatar">
+    <h2 v-if="faveLang">😍 Favourite Language: {{ faveLang }}</h2>
+    <h3 v-if="followers.length > 0">Followers ({{ followers.length }}):</h3>
+    <ul v-if="followers.length > 0">
+      <li v-for="follower in followers">
+        {{ follower }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -18,24 +22,91 @@ import axios from 'axios'
 export default {
   data () {
     return {
+      inputValue: '',
       username: '',
-      data: {},
-      errorMsg: ''
+      avatar: '',
+      followers: [],
+      faveLang: '',
+      errorMsg: '',
+      urlBase: 'https://api.github.com/users'
     }
   },
   methods: {
-    search () {
-      const api = `https://api.github.com/users/${this.username}`
+    submit () {
+      const api = `${this.urlBase}/${this.inputValue}`
 
+      this.fetchUser(api)
+    },
+    fetchUser (api) {
       axios.get(api)
       .then(res => {
-        this.data = res.data
+        const { data } = res
+
+        this.inputValue = ''
+        this.username = data.login
+        this.avatar = data.avatar_url
+
+        this.fetchFollowers()
+        this.fetchFaveLang()
       })
-      .catch(() => {
-        this.errorMsg = 'No user or no location!'
-        this.data = {}
+      .catch(err => {
+        console.warn('ERROR:', err)
+      })
+    },
+    fetchFollowers () {
+      axios.get(`${this.urlBase}/${this.username}/followers`)
+      .then(res => {
+        this.followers = res.data.map(follower => follower.login)
+      })
+    },
+    fetchFaveLang () {
+      axios.get(`${this.urlBase}/${this.username}/repos`)
+      .then(res => {
+        const langs = res.data.map(repo => repo.language)
+
+        var map = langs.reduce((prev, next) => {
+          if (!prev[next]) {
+            prev[next] = 0
+          }
+
+          prev[next] += 1
+
+          return prev
+        }, {})
+
+        var faveLang = Object.keys(map).reduce((prev, next) => {
+          return map[prev] > map[next] ? prev : next
+        })
+
+        this.faveLang = faveLang
       })
     }
   }
 }
 </script>
+
+<style lang="stylus">
+body
+  background-color goldenrod
+
+.fetch
+  dispaly flex
+  align-items center
+  flex-flow column
+  font-family Comic Sans MS
+
+  input
+    width 320px
+
+  input,
+  button
+    font-size 25px
+
+  h1
+    font-size 44px
+
+  .avatar
+    height 200px
+    width 200px
+    border-radius 10%
+</style>
